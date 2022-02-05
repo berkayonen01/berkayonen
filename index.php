035e05b480200f2ba7a521423e6bb69bd878b5dc
Trjokeer01 <97757854+Trjokeer01@users.noreply.github.com>
2022-02-05 19:54:59 +0300

    ilk gönderme

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anasayfa</title>
+    <script src="https://kit.fontawesome.com/c20485228a.js" crossorigin="anonymous"></script>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+</head>
+<body>
+    <section id="menu">
+        <div id="logo">Önenler Yazılım </div>
+  
+    <nav>
+        <a href="#menu"><i class="fas fa-home ikon"></i>Anasayfa</a>
+        <a href="#hakkımızda"><i class="fas fa-info ikon"></i>Hakkımızda</a>
+        <a href="#eğitimler"><i class="fas fa-graduation-cap ikon"></i>Eğitimler</a>
+        <a href="#ekip"><i class="fas fa-user-friends ikon"></i>Ekip</a>
+        <a href="#iletişim"><i class="fas fa-phone-alt ikon"></i>İletişim</a>
+    </nav> 
+    </section>
+    <section id="barner"> 
+        <div id="black">
+        </div>  
+        <div id="içerik">
+            <h2>Önenler Yazılım</h2>
+            <hr width=300 align=left>
+            <p>Bu sitenin amacı sadece test amaçlı olup hiç bir kuruluşa bağımlı değildir önenler yazılım iyi gezmeler diler eğer bir gün tanışma fırsatımız olursa sizi şirketimize davet ederim iyi günler.<br>
+                Bu sitenin kuruluş amacı şirketimizi tanımanız ve benimsemenizdir biz Önenler yazılım olarak bilgileriniz güvenliğinden sorumluyuz bu yüzden bu siteye güvenebilir ve giris yapabilirsiniz ilk yaptımız site olduğu için böyle kötü gözüke bilir ama ilerde daha güzel bir siteyle geleceğiz.
+
+                Bu yazılım sitesi genç yazılımcılara örnek olsun diye hazırlanmıştır ve genç yazılımcılar iletişim kısmından bize ulaşıp bizim şirkete katılabilirsiniz kapımız herzaman açık.</p>
+        </div>
+    </section>
+    <section id="hakkımızda">
+        <h3>Hakkımızda</h3>
+        <div class="container">
+            <div id="sol">
+            <h5 id="h5sol">
+                Biz 2021 ağustos aynın 2.günü kurulduk.Biz Önenler yazılım olarak gizlilik ve güvenlik üzerine kurulmuş bir şirketiz.
+            </h5>
+        </div>
+        <div id="sağ">
+            <span>S</span>
+            <p id="psağ">istem yazılımları Bilgisayarın kendisinin işletilmesini sağlayan, işletim sistemi, derleyiciler (compilers) (Yazılım programında, yazılan programı makine diline çeviren program), çeşitli donatılar (facility) gibi yazılımlardır. Çekirdek(kernel) işletim sisteminin en temel parçasıdır. Burada çekirdek ile ilgili farklı yaklaşımlar olduğunu yani yazılım karar verme ve Programlama paradigması mevcut olsa da bir işletim sistemi çekirdeğinin esas görevi Bilgisayar Donanımı ile kullanıcıya ve kullanıcıya yazılım veya donanım üreten üreticilere Arayüz oluşturmak ve kaynakların yönetilmesi ile ilgili birimleri idare etmektir.</p>
+        </div>
+        <img src="img/y1.jpg" alt="" class="img-fluid mt100">
+        <p id="pson">Uygulama yazılımları Bu kullanıcıların işlerine çözüm sağlayan örneğin çek, senet, stok kontrol, bordro, kütüphane kayıtlarını tutan programlar, bankalardaki müşterilerin para hesaplarını tutan programlar gibi yazılımlardır. Bütün sistem programları içinde en temel yazılım işletim sistemidir ki, bilgisayarın bütün donanım ve yazılım kaynaklarını kontrol ettiği gibi, kullanıcılara ait uygulama yazılımlarının da çalıştırılmalarını ve denetlenmelerini sağlar. Çevirici yazılımlar Herhangi bir dilde yazılan programı makine diline çeviren yazılımlardır. Yazılım çeşitleri Sistem yazılımları Sistem yazılımları bilgisayar kullanımı için gerekli ana fonksiyonları sağlar ve bilgisayar donanımına ve sistemin yürütülmesine yardımcı olur. Şu kombinasyonları içerir: Aygıt sürücüleri İşletim sistemleri Sunucular Hizmet programları Pencere sistemleri Sistem yazılımı çeşitli bağımsız donanım bileşenlerinin uyum içinde çalışmalarından sorumludur. Sistem yazılımı bilgisayar donanımının işletilmesi ve uygulama yazılımının çalıştırılması için bir platform sağlamak için tasarlanmış bir bilgisayar yazılımıdır. En temel sistem yazılımı türleri şunlardır: Bilgisayar BIOS’u ve aygıt yazılımı: Bilgisayara bağlı veya bilgisayar içindeki donanımı çalıştırmak ve kontrol etmek için gereken temel işlevselliği sağlar. İşletim sistemi ( önde gelen örnekler; Microsoft Windows, Mac OS X ve Linux olmak üzere): Bilgisayar parçalarının; hafıza ile diskler arasında veri alışverişi veya monitöre görüntü sağlamak gibi görevleri uygulayarak birlikte çalışmasına olanak sağlar. Ayrıca üst düzey sistem yazılımı ve uygulama yazılımlarının çalıştırılması için bir platform oluşturur. Yardımcı yazılım: Bilgisayarın analiz edilmesine, yapılandırılmasına, yönetilmesine ve optimize edilmesine yardımcı olur. Ayrıca sistem yazılımı terimi, bazı yayınlarda yazılım geliştirme araçlarını tanımlamak için de kullanılır. Bilgisayar alıcıları nadiren sahip olduğu işletim sistemini öncelikli olarak dikkate alarak bir bilgisayar alırlar. Fakat cep telefonu gibi aygıtları satın alan kişiler için bu durumun tersi geçerli olabilir. Çünkü iPhone örneğinde olduğu gibi bu tür aygıtların sistem yazılımlarının, son kullanıcı tarafından değiştirilmesi oldukça zordur. Ayrıca sistem yazılımı genellikle dahili ya da önceden yüklenmiş şekilde, yararlı ve hatta gerekli bir alt yapı kodu olarak görev yapar. Sistem yazılımının dışında, kullanıcıların dokümanlar oluşturmasına, oyun oynamasına, müzik dinlemesine ya da İnternet'te gezinmesine olanak sağlayan yazılımlara uygulama yazılımı denir.</p>
+        </div>
+    </section>
+    <section id="eğitimler">
+        <div class="container">
+            <h3>Eğitimler</h3>
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">HTML5 ve CSS3 eğitimi</h5>
+                    <p class="cardp">Burada HTML5 ve CSS3 eğitimerimiz bulunmaktadır</p>
+                </div>
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">Javasicript eğitimi</h5>
+                    <p class="cardp">Burada Javasicript eğitimerimiz bulunmaktadır</p>
+                </div>
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">Python eğitimi</h5>
+                    <p class="cardp">Burada Python eğitimerimiz bulunmaktadır</p>
+                </div>
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">Java</h5>
+                    <p class="cardp">Burada Java eğitimerimiz bulunmaktadır</p>
+                </div>
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">C++ eğitimi</h5>
+                    <p class="cardp">Burada C++ eğitimerimiz bulunmaktadır</p>
+                </div>
+                <div class="card item" data-merge=2>
+                    <img src="img/r2.jpg" alt="" class="img-fluid">
+                    <h5 class="başlıkcard">C# eğitimi</h5>
+                    <p class="cardp">Burada C# eğitimerimiz bulunmaktadır</p>
+
+                </div>
+        </div>
+        </div>
+    </section>
+    <section id="ekip">
+        <div class="container">
+            <h3 id="ekiph3">Ekip</h3>
+
+                <div class="sutun-sol-sağ">
+
+                    <img src="img/profil.jpg" class="img-fluid oval">
+                    <h4 class="ekipisim">Efe ÖNEN</h4>
+                    <p class="ekipp">Yardımcı seo.</p>
+                       <div class="ekip-ikon">
+                            <a href="#"><i class="fab fa-instagram sosical"></i></a>
+                            <a href="#"><i class="fab fa-facebook-f sosical"></i></a>
+                            <a href="#"><i class="fab fa-twitter sosical"></i></a>
+                        </div> 
+                </div>
+                </div>
+                    <div class="sutun-sol-sağ">
+
+                        <img src="img/profil.jpg" class="img-fluid oval">
+                        <h4 class="ekipisim">Berkay ÖNEN</h4>
+                        <p class="ekipp">Şirketin kurucu seosu ve sayfanın kodlarını yazan kişi.</p>
+                           <div class="ekip-ikon">
+                                <a href="#"><i class="fab fa-instagram sosical"></i></a>
+                                <a href="#"><i class="fab fa-facebook-f sosical"></i></a>
+                                <a href="#"><i class="fab fa-twitter sosical"></i></a>
+                            </div> 
+                        </div>
+                        <div class="sutun-sol-sağ">
+
+                            <img src="img/profil.jpg" class="img-fluid oval">
+                            <h4 class="ekipisim">Kayra  YILDIRIM</h4>
+                            <p class="ekipp">Şirketin kurucu ortağı.</p>
+                               <div class="ekip-ikon">
+                                    <a href="#"><i class="fab fa-instagram sosical"></i></a>
+                                    <a href="#"><i class="fab fa-facebook-f sosical"></i></a>
+                                    <a href="#"><i class="fab fa-twitter sosical"></i></a>
+                                </div> 
+                            </div>                  
+        </div>
+    </section>
+    <section id="iletişim">
+        <div class="container"> 
+            <h3 id="h3iletişim">İletişim</h3>
+            <form action="index.php" method="post">
+            <div id="iletişimopak">
+                <div id="formgroup"> 
+                    <div id="solform">
+                     <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                     <input type="tel" name="tel" placeholder="Telefon numarası" required class="form-control">
+                    </div>
+                     <div id="sağform">
+                      <input type="email" name="mail" placeholder="E-mail adresi" required class="form-control">
+                      <input type="text" name="konu" placeholder="Konu başlığı" required class="form-control">
+                     </div>
+                     <textarea name="mesaj" id="" cols="30" rows="10" placeholder="Mesaj girin" required class="form-control"></textarea>
+                     <input type="submit" value="Gönder" class="submit">
+            
+                </div> 
+                <div id="adres">
+                <h4 id="adresbaşlık">Adres :</h4>
+                <p class="adresp">Mehmet Akif Ersy Mah.</p>
+                <p class="adresp">Akyıldız caddesi</p>
+                <p class="adresp">Oguz Bey sokak No:123</p>
+                <p class="adresp">0212 389 99 99</p>
+                <p class="adresp">E-mail :onenleryazilim@hotmail.com</p>
+            
+                </div>
+            </div>
+            </form>
+            <footer>
+                <div id="copyright">2021 | Tüm hakarı saklıdır</div>
+                <a href="#"><i class="fab fa-instagram sosical"></i></a>
+                                <a href="#"><i class="fab fa-facebook-f sosical"></i></a>
+                                <a href="#"><i class="fab fa-twitter sosical"></i></a>
+                <div id="sosicalfooter">
+                </div>
+                <a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+            </footer>
+        </div>
+    </section>
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+  integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+  crossorigin="anonymous"></script>
+  <script src="jquery.min.js"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="owl/sicript.js"></script>
+</body>
+</html>
+<?php
+include("baglanti.php");
+if(isset($_POST["isim"],$_POST["tel"],$_POST["mail"],$_POST["konu"],$_POST["mesaj"]))
+{
+  $adsoyad=$_POST["isim"];
+  $telefon=$_POST["tel"];
+  $email=$_POST["mail"];
+  $konu=$_POST["konu"];
+  $mesaj=$_POST["mesaj"];
+  $ekle="INSERT INTO iletisim (adsoyad,telefon,email,konu,mesaj) VALUES ('".$adsoyad."','".$email."','".$telefon."','".$konu."','".$mesaj."')";
+}
+if ($baglan->query($ekle) === TRUE) 
+{
+    echo "<script> alert(`Mesajınız başarı ile gönderilmiştir.`) </script>";
+}
+else 
+{
+   echo "Hata: " . $ekle. "<br>" . $baglan->error;
+}
